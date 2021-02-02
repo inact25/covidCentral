c8399d8e9ba3003db625762432b472516cc22094
Angga Adji Surya <anggas25@live.com>
2021-02-02 09:30:15 +0700

    Styling symptom section

--- src/sections/Symptom.js
+++ src/sections/Symptom.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import symptompsImg from "../assets/img/symptoms.png"
-import {AiOutlineInfoCircle,AiFillCheckCircle} from "react-icons/all"
+import {AiFillCheckCircle, AiOutlineInfoCircle} from "react-icons/all"
 
 class Symptom extends Component {
     render() {
@@ -9,36 +9,41 @@ class Symptom extends Component {
                 <div className="container py-5 my-5">
                     <div className="row h-100">
                         <div className="col-12 col-md-6 col-lg-6 order-2 order-md-1 order-lg-1">
-                            <div className="hero-badge">
-                                <button type="button" className="btn btn-sm btn-outline-danger rounded-pill mb-2">
-                                    <span style={{fontSize:"1.15rem"}}><AiOutlineInfoCircle/></span> Covid-19 Alert
-                                </button>
+                            <p className="hero-badge">
+                                <AiOutlineInfoCircle/>   Covid-19 Alert
+                            </p>
+                            <div className="symptom-title">
+                                <h2>
+                                    What is novel coronavirus?
+                                </h2>
                             </div>
-                            <h2>
-                                What is novel coronavirus?
-                            </h2>
-                            <p>COVID-19 Symptoms are from mild to severe. It takes 2-14 days after exposure for symptoms to develop. Symptoms may include:</p>
-                            <div className="row">
-                                <div className="col-12 col-md-6 col-lg-6 mb-2">
-                                    <span className="text-success"><AiFillCheckCircle/></span> Hard Cought
-                                </div>
-                                <div className="col-12 col-md-6 col-lg-6 mb-2">
-                                    <span className="text-success"><AiFillCheckCircle/></span> Resoiratory Distress
-                                </div>
-                                <div className="col-12 col-md-6 col-lg-6 mb-2">
-                                    <span className="text-success"><AiFillCheckCircle/></span> Fever
-                                </div>
-                                <div className="col-12 col-md-6 col-lg-6 mb-2">
-                                    <span className="text-success"><AiFillCheckCircle/></span> Shortness of Breath
-                                </div>
-                                <div className="col-12 col-md-6 col-lg-6 mb-2">
-                                    <span className="text-success"><AiFillCheckCircle/></span> Headache
-                                </div>
-                                <div className="col-12 col-md-6 col-lg-6 mb-2">
-                                    <span className="text-success"><AiFillCheckCircle/></span> Kidney Failure
+                            <div className="symptom-description">
+                                <p>COVID-19 Symptoms are from mild to severe. It takes 2-14 days after exposure for
+                                    symptoms to develop. Symptoms may include:</p>
+                            </div>
+                            <div className="symptom-list">
+                                <div className="row">
+                                    <div className="col-12 col-md-6 col-lg-6 mb-3">
+                                        <span className="text-success"><AiFillCheckCircle/></span> Hard Cought
+                                    </div>
+                                    <div className="col-12 col-md-6 col-lg-6 mb-3">
+                                        <span className="text-success"><AiFillCheckCircle/></span> Resoiratory Distress
+                                    </div>
+                                    <div className="col-12 col-md-6 col-lg-6 mb-3">
+                                        <span className="text-success"><AiFillCheckCircle/></span> Fever
+                                    </div>
+                                    <div className="col-12 col-md-6 col-lg-6 mb-3">
+                                        <span className="text-success"><AiFillCheckCircle/></span> Shortness of Breath
+                                    </div>
+                                    <div className="col-12 col-md-6 col-lg-6 mb-3">
+                                        <span className="text-success"><AiFillCheckCircle/></span> Headache
+                                    </div>
+                                    <div className="col-12 col-md-6 col-lg-6 mb-3">
+                                        <span className="text-success"><AiFillCheckCircle/></span> Kidney Failure
+                                    </div>
                                 </div>
                             </div>
-                            <div className="hero-action mb-2">
+                            <div className="hero-action mt-5 mb-2">
                                 <button className="btn-danger btn-lg btn rounded-pill">Check Symptom</button>
                             </div>
                         </div>
