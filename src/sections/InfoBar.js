a572aeedeb6aedb9df3d40e9f8fd28ef3af69da4
Angga Adji Surya <anggas25@live.com>
2021-02-23 21:27:52 +0700

    fix InfoBar.js responsive in small device

--- src/sections/InfoBar.js
+++ src/sections/InfoBar.js
@@ -35,13 +35,17 @@ class InfoBar extends Component {
                 <div className="container infobar-container pt-5 pb-4 px-5 text-center">
                     {this.state.isLoaded ?
                         <div className="row">
-                            <div className="col-12 col-md-3 col-lg-3 text-danger"><h3>{nFormatter(data.confirmed)}</h3>
+                            <div className="col-12 col-sm-6 col-md-3 col-lg-3 text-danger">
+                                <h3>{nFormatter(data.confirmed)}</h3>
                                 <p>Confirmed Case</p></div>
-                            <div className="col-12 col-md-3 col-lg-3 text-danger"><h3>{nFormatter(data.critical)}</h3>
+                            <div className="col-12 col-sm-6 col-md-3 col-lg-3 text-danger">
+                                <h3>{nFormatter(data.critical)}</h3>
                                 <p>Critical Case</p></div>
-                            <div className="col-12 col-md-3 col-lg-3 text-success"><h3>{nFormatter(data.recovered)}</h3>
+                            <div className="col-12 col-sm-6 col-md-3 col-lg-3 text-success">
+                                <h3>{nFormatter(data.recovered)}</h3>
                                 <p>Recovered Case</p></div>
-                            <div className="col-12 col-md-3 col-lg-3 text-danger"><h3>{nFormatter(data.deaths)}</h3>
+                            <div className="col-12 col-sm-6 col-md-3 col-lg-3 text-danger">
+                                <h3>{nFormatter(data.deaths)}</h3>
                                 <p>Death Case</p></div>
                         </div>
                         : <div><img src={Loading} alt="" height={100}/><p>Loading...</p></div>
