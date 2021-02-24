be0302bf5f4c8c7291fab73ce526c4e9fe076a40
Angga Adji Surya <anggas25@live.com>
2021-02-24 13:23:10 +0700

    migrating to NextJs

--- src/services/config/Base.js
+++
@@ -1,2 +0,0 @@
-export const accessToken = "5cf9dfd5-3449-485e-b5ae-70a60e997864"
-export const baseUrl = "https://covid19-api.com/"
--- src/services/tools/CFormater.js
+++
@@ -1,3 +0,0 @@
-export function CFormater(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
